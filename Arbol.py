149fc8f7f1d1b221d10548522188f4429b999863
ALAN ENRIQUE ENRIQUE <>
2022-10-18 20:14:59 +0000

    Initial commit

---
+++ Arbol.py
@@ -0,0 +1,47 @@
+import nodo from Nodo
+class Arbol:
+  #Constructor
+  def__init__(self,dato):
+    self.raiz = Nodo(dato)
+  ###########Funciones privadas###########
+  #Agregar un nodo al árbol
+  def __agregar(self,nodo,dato):
+    if dato<nodo.dato:
+      if nodo.izq is None:
+        nodo.izq = Nodo(dato)
+      else:
+        self.__agregar(nodo.izq,dato)
+    else:
+      if dato.der is None: 
+        nodo.der = Nodo(dato)
+      else:
+        self.__agregar(nodo.der,dato)
+
+  #Recorrer árbol inorden      
+  def __inorden(self,nodo):
+    if nodo is not None:
+      self.__inorden(nodo.izq)
+      print(nodo.dato,end=", ")
+      self.__inorden(nodo.derecha)
+  #Recorrer árbol preorden    
+  def __preorden(self,nodo):
+    if nodo is not None: 
+      print(nodo.dato,end=", ")
+      self.__inorden(nodo.izq)
+      self.__inorden(nodo.derecha)
+      #Recorrer árbol postorden    
+  def __postorden(self,nodo):
+    if nodo is not None: 
+      self.__inorden(nodo.izq)
+      self.__inorden(nodo.derecha)
+      print(nodo.dato,end=", ")
+
+  def __busqueda_avara(self,nodo,busqueda):
+    print("Hola")
+  ###########Funciones publicas###########  
+def agregar(self,dato): 
+  self.__agregar(self.raiz,dato)
+def inorden(self,dato):
+  print("Recorrido In Orden del árbol: ")
+  self.__inorden(self.raiz,dato)
+  print(" ")  
